9ec178d218e0a3b8298271a84eff0b74286e5873
Sai Lokesh Chirumarri <76848659+sailokesh-hub@users.noreply.github.com>
2024-02-02 17:25:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,9 @@
 const express = require("express");
-const mysql = require("mysql2");
 const cors = require("cors");
-const path = require("path");
 const port = process.env.PORT || 3001;
 
-const bodyParser = require("body-parser");
+const app = express();
 
-
-const initializeDbAndServer = async () => {
-    try {
-      connection.connect((err) => {
-        if (err) {
-          console.error("Error connecting to MySQL database: ", err);
-          return;
-        }
-        console.log("Connected to MySQL database");
-      });
-      app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-    } catch (error) {
-      console.log(`Data base Error is ${error}`);
-      process.exit(1);
-    }
-  };
-  
-  initializeDbAndServer();
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 app.get("/", (req, res) => res.send("hello world"));
-
